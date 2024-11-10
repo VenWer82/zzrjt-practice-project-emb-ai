093d1a14066833c5ccfda1929a54485f0d7cef7f
VenWer82 <werven321@gmail.com>
2024-11-10 14:23:42 -0500

    final on analyze route

--- server.py
+++ server.py
@@ -3,6 +3,7 @@
     localhost:5000.
 '''
 from flask import Flask, render_template, request
+from SentimentAnalysis.sentiment_analysis import sentiment_analyzer
 # Import Flask, render_template, request from the flask pramework package : TODO
 # Import the sentiment_analyzer function from the package created: TODO
 app = Flask('Sentiment Analyzer')
@@ -15,7 +16,18 @@ def sent_analyzer():
         function. The output returned shows the label and its confidence 
         score for the provided text.
     '''
-   
+    try:
+      text_to_analyze = request.args.get('textToAnalyze')
+      if text_to_analyze is None:
+          return {'message': 'Missing paramater'}, 400
+
+      result = sentiment_analyzer(text_to_analyze)   
+      label = result['label']
+      score = result['score']
+      return f'The analyzed text is of sentiment {label}, and the score is {score}'
+    except Exception as e:
+        print(e, text_to_analyze)
+        return {'message': 'Internal server error'}, 500
 
 @app.route("/")
 def render_index_page():
@@ -27,4 +39,4 @@ def render_index_page():
 if __name__ == "__main__":
     ''' This functions executes the flask app and deploys it on localhost:5000
     '''
-    app.run(host='0.0.0.0',port=5000)
+    app.run(host='0.0.0.0',port=5000, debug=True)
